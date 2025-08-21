18f461c83405ff78350878eb1641c9f6ed29abe3
ChenWei5812 <101046326+ChenWei5812@users.noreply.github.com>
2025-08-21 13:48:44 +0800

    Update analyze_font.sh

--- scripts/analyze_font.sh
+++ scripts/analyze_font.sh
@@ -4,7 +4,7 @@ TARGET_FONT_PATH="fonts/myfont.ttf"
 REFERENCE_FONTS_DIR="fonts/jigmo/"
 
 
-python analyze_font.py \
+python3 analyze_font.py \
     --target_font_path "$TARGET_FONT_PATH" \
     --reference_fonts_dir "$REFERENCE_FONTS_DIR" \
     --analyze_target_font \
