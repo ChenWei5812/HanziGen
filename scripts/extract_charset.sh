ae53a0cc6afa8909ad5e212382a0206c22d909c0
ChenWei5812 <101046326+ChenWei5812@users.noreply.github.com>
2025-08-21 13:49:37 +0800

    Update extract_charset.sh

--- scripts/extract_charset.sh
+++ scripts/extract_charset.sh
@@ -7,7 +7,7 @@ RANDOM_SEED=2025
 DEVICE="cuda"
 
 
-python extract_charset.py \
+python3 extract_charset.py \
     --target_font_path "$TARGET_FONT_PATH" \
     --split_ratios "$TRAIN_SPLIT_RATIO" "$VAL_SPLIT_RATIO" \
     --random_seed "$RANDOM_SEED" \
