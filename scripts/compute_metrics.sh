7cfa5927bd049c933d11c6e94324313ca2f4b26e
ChenWei5812 <101046326+ChenWei5812@users.noreply.github.com>
2025-08-21 13:49:02 +0800

    Update compute_metrics.sh

--- scripts/compute_metrics.sh
+++ scripts/compute_metrics.sh
@@ -9,7 +9,7 @@ TARGET_FONT_NAME=$(basename "$TARGET_FONT_PATH" | sed -E 's/\.(ttf|otf)$//')
 GENERATED_IMG_DIR="samples_${TARGET_FONT_NAME}/eval_outputs/gen"
 GROUND_TRUTH_IMG_DIR="samples_${TARGET_FONT_NAME}/eval_outputs/gt"
 
-python compute_metrics.py \
+python3 compute_metrics.py \
     --generated_img_dir "$GENERATED_IMG_DIR" \
     --ground_truth_img_dir "$GROUND_TRUTH_IMG_DIR" \
     --eval_batch_size "$EVAL_BATCH_SIZE" \
